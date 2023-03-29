4f18b2858e59c2ac2968eff2b13aa0a4a646baf3
Pooja Patel <31209808+patelpooja1998@users.noreply.github.com>
2023-03-28 22:36:13 -0400

    Added Day1 code

---
+++ Day01/day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band name generator!")
+city = input("What city did you grow up in ?")
+pet = input("What is the name of your first pet ?")
+print("Your band name is " + city +" "+pet)
